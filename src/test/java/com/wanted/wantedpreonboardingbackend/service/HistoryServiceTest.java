0340566dcf1677b6d799eef472a53f82b6a78175
mooncy0809 <mooncy0809@naver.com>
2023-10-08 18:53:27 +0900

    test(api): 테스트 코드 추가

--- src/test/java/com/wanted/wantedpreonboardingbackend/service/HistoryServiceTest.java
+++ src/test/java/com/wanted/wantedpreonboardingbackend/service/HistoryServiceTest.java
@@ -1,60 +1,41 @@
 package com.wanted.wantedpreonboardingbackend.service;
 
+import com.wanted.wantedpreonboardingbackend.domain.History;
 import com.wanted.wantedpreonboardingbackend.domain.JobPosting;
 import com.wanted.wantedpreonboardingbackend.domain.User;
 import com.wanted.wantedpreonboardingbackend.dto.HistoryDTO;
-import com.wanted.wantedpreonboardingbackend.exception.DuplicateException;
 import com.wanted.wantedpreonboardingbackend.repository.HistoryRepository;
-import com.wanted.wantedpreonboardingbackend.repository.JobPostingRepository;
-import com.wanted.wantedpreonboardingbackend.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class HistoryServiceTest {
+    @Autowired
+    private HistoryService historyService;
+
     @Mock
     private HistoryRepository historyRepository;
 
-    @Mock
-    private JobPostingRepository jobPostingRepository;
-
-    @Mock
-    private UserRepository userRepository;
-
-    @InjectMocks
-    private HistoryService historyService;
-
+    @DisplayName("applyToJob: 채용 지원 성공.")
     @Test
     void testApplyToJob() {
         HistoryDTO historyDTO = new HistoryDTO();
         historyDTO.setJobPostingId(1);
         historyDTO.setUserId(1);
 
-        when(historyRepository.existsByJobPostingIdAndUserId(1, 1)).thenReturn(false);
-
-        JobPosting jobPosting = new JobPosting();
-        when(jobPostingRepository.findById(1)).thenReturn(java.util.Optional.of(jobPosting));
-        User user = new User();
-        when(userRepository.findById(1)).thenReturn(java.util.Optional.of(user));
+        when(historyRepository.save(any(History.class))).thenAnswer(invocation -> {
+            Object savedHistory = invocation.getArgument(0);
+            return savedHistory;
+        });
 
         HistoryDTO savedHistoryDTO = historyService.applyToJob(historyDTO);
 
         assertNotNull(savedHistoryDTO);
-        assertEquals(1, savedHistoryDTO.getJobPostingId());
-        assertEquals(1, savedHistoryDTO.getUserId());
-    }
-
-    @Test
-    void testDuplicateException() {
-        HistoryDTO historyDTO = new HistoryDTO();
-        historyDTO.setJobPostingId(1);
-        historyDTO.setUserId(1);
-
-        when(historyRepository.existsByJobPostingIdAndUserId(1, 1)).thenReturn(true);
-
-        assertThrows(DuplicateException.class, () -> historyService.applyToJob(historyDTO));
     }
 }
